f61a82062d3d1e235a4607663453f95b54f088de
JACFlyer <58670895+JACFlyer@users.noreply.github.com>
2020-03-12 17:01:20 -0600

    Fragment classes created.

---
+++ app/src/main/java/edu/cnm/deepdive/aprilv2/controller/AssessmentFragment.java
@@ -0,0 +1,30 @@
+package edu.cnm.deepdive.aprilv2.controller;
+
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import edu.cnm.deepdive.aprilv2.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class AssessmentFragment extends Fragment {
+
+
+  public AssessmentFragment() {
+    // Required empty public constructor
+  }
+
+
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+      Bundle savedInstanceState) {
+    // Inflate the layout for this fragment
+    return inflater.inflate(R.layout.fragment_assessment, container, false);
+  }
+
+}
